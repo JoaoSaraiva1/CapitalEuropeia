388c79743ca5b3261b8bce92dd2782671542dda0
JoaoSaraiva1 <48354004+JoaoSaraiva1@users.noreply.github.com>
2020-11-03 11:18:03 +0000

    Add project files.

---
+++ CapitalEuropeia/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CapitalEuropeia.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
